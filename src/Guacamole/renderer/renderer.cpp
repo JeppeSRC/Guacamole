049dce201994916a4f7bfc9daa2ddfc3a94f5729
JeppeSRC <theholyhorse.dev@gmail.com>
2022-10-23 21:13:14 +0200

    Why u suck microsoft

--- src/Guacamole/renderer/renderer.cpp
+++ src/Guacamole/renderer/renderer.cpp
@@ -22,6 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#include <Guacamole.h>
+
 #include "renderer.h"
 
 #include <Guacamole/vulkan/commandpoolmanager.h>
